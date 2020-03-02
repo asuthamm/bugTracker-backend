e235ea718df1b14cab5cde13743a11fb1f3360a1
asuthamm <asuthamm@gmail.com>
2020-03-01 22:54:19 -0500

    correct environment setting

--- Gemfile
+++ Gemfile
@@ -44,3 +44,5 @@ gem "jwt", "~> 2.2"
 
 gem "active_model_serializers", "~> 0.10.10"
 gem 'cloudinary'
+
+gem 'sendgrid-ruby'
--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'asuthamm',
-  :password => 'Endevor!98',
+  :user_name => '',
+  :password => '',
   :domain => 'myBugTracker.com',
   :address => 'smtp.sendgrid.net',
   :port => 465,
