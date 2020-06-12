997de72aca5851c448fb782e8c18915e79e0c54c
asuthamm <asuthamm@gmail.com>
2020-06-12 16:42:13 -0400

    new Gemfile.lock to addr security

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,8 @@ alex = User.create(username: 'suthamm', password: '123', name: 'Alex Suthammanon
 proj1 = Project.create(name: 'bugTracker', description: 'A simple bug tracking app that helps you manage bugs easily', user: alex)
 proj2 = Project.create(name: 'Broadway App', description: 'Mod-3 project', user: alex)
 
-# tix1 = Ticket.create(title: 'Bug-1', description: 'This is my first bug.', deadline: "2020/10/20", status: 'New', category: 'Bug', attach_link: 'https://ca.slack-edge.com/T02MD9XTF-UQLTN7R37-dcc5ff846fc6-48', project: proj1)
-# tix2 = Ticket.create(title: 'Bug-2', description: 'front-end broken', deadline: "2020/10/20", status: 'New', category: 'Frontend', attach_link: 'https://ca.slack-edge.com/T02MD9XTF-UQLTN7R37-dcc5ff846fc6-48', project: proj1)
+tix1 = Ticket.create(title: 'Bug-1', description: 'This is my first bug.', deadline: "2020/10/20", status: 'New', category: 'Bug', attach_link: 'https://ca.slack-edge.com/T02MD9XTF-UQLTN7R37-dcc5ff846fc6-48', project: proj1)
+tix2 = Ticket.create(title: 'Bug-2', description: 'front-end broken', deadline: "2020/10/20", status: 'New', category: 'Frontend', attach_link: 'https://ca.slack-edge.com/T02MD9XTF-UQLTN7R37-dcc5ff846fc6-48', project: proj1)
 
 john = User.create(username: 'john', password: '123', name: 'John Freedom', role: 'Developer', email: 'john@email.com', img_url: 'https://p7.hiclipart.com/preview/980/886/491/computer-icons-icon-design-avatar-flat-face-icon.jpg')
 proj3 = Project.create(name: 'Yelp App', description: 'A Yelp look-alike', user: john)
